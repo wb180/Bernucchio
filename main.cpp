cec688c116a72839043bedd95e1130cbf5e448b6
wb180 <wirbrauchen180@gmail.com>
2022-09-17 15:30:53 +0300

    Fix cmake build options

--- main.cpp
+++ main.cpp
@@ -1,36 +1,13 @@
-#include <iostream>
-#include <ctime>
-
 #include "moves.h"
 #include "uci.h"
-#include "gamestate.h"
-#include "optimization.h"
-
-#ifdef BUILD_WITH_TESTS
-    #include <gtest/gtest.h>
-#endif
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
     InitializeBishopMoves();
     InitializeRookMoves();
 
-#ifdef BUILD_WITH_TESTS
-    testing::InitGoogleTest(&argc, argv);
-    int result = RUN_ALL_TESTS();
-    if(result)
-        return result;
-    else
-    {
-#endif
+    UCI uci;
+    uci.Loop();
 
-        //optimize();
-
-        UCI uci;
-        uci.Loop();
-
-#ifdef BUILD_WITH_TESTS
-    }
-#endif
     return 0;
 }
